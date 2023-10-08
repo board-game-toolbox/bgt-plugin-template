12ae656917a818222ec6f8b24d500645f70f9809
haoxingwei <haoxingwei@eastmoney.com>
2023-10-08 15:43:50 +0800

    feat: using legacy build

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import type { InputPluginOption } from "rollup";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import type { InputPluginOption } from 'rollup';
+import legacy from '@vitejs/plugin-legacy';
 
 /**
  * change all `"/[url]"` behind `src` and `href` to `"./[url]"`
  */
 function RelativePathPlugin(): InputPluginOption {
   return {
-    name: "useRelativePathPlugin",
+    name: 'useRelativePathPlugin',
     generateBundle(_, bundle) {
       for (const file of Object.keys(bundle)) {
-        if (file.indexOf("index.html") !== -1) {
+        if (file.indexOf('index.html') !== -1) {
           const asset = bundle[file];
           // @ts-ignore asset.source is not in type
           asset.source = asset.source.replaceAll(
@@ -25,9 +26,14 @@ function RelativePathPlugin(): InputPluginOption {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+      renderModernChunks: false,
+    }),
+  ],
   build: {
-    target: "ES2015",
     rollupOptions: {
       plugins: [RelativePathPlugin()],
     },
